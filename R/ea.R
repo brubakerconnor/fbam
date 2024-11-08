c3fd56bf75b5c9cabefb0c426a5e884185097a3e
Connor Brubaker <brubaker@tamu.edu>
2024-11-08 01:54:36 -0600

    Renaming to EA; using ggplot

--- R/ea.R
+++ R/ea.R
@@ -159,51 +159,91 @@ ea <- function(X, nbands, nsubpop, popsize, maxgen, maxrun, tol,
     params = list(nsubpop = nsubpop, nbands = nbands, popsize = popsize,
                   pmutate = pmutate, maxgen = maxgen, maxrun = maxrun,
                   tol = tol, sample_rate = sample_rate))
-  class(ga) <- "ga_output"
+  class(ga) <- "ea_output"
   return(ga)
 }
 
 #' @export
-print.ga_output <- function(ga) {
-  cat('Number of replicates provided:', ncol(ga$spec), '\n')
-  cat('nsubpop = ', ga$params$nsubpop, ', nbands = ', ga$params$nbands, sep = "")
-  for (j in 1:ga$params$nsubpop) {
-    nrep_subpop <- ncol(ga$spec[, ga$labels == j, drop = FALSE])
+print.ea_output <- function(ea) {
+  cat('Number of replicates provided:', ncol(ea$spec), '\n')
+  cat('nsubpop = ', ea$params$nsubpop, ', nbands = ', ea$params$nbands, sep = "")
+  for (j in 1:ea$params$nsubpop) {
+    nrep_subpop <- ncol(ea$spec[, ea$labels == j, drop = FALSE])
     cat('\n\nSubpopulation j =', j, '\nNumber of replicates:', nrep_subpop,
         '\n\tBoundaries:',
-        paste0(round(ga$endpoints[j,], 4), ' Hz'),
-        '\n\tAverage Summary Measures:', round(ga$avg_summary[j,], 2))
+        paste0(round(ea$endpoints[j,], 4), ' Hz'),
+        '\n\tAverage Summary Measures:', round(ea$avg_summary[j,], 2))
   }
 }
 
 #' @export
-plot.ga_output <- function(ga, type = 'solution') {
+plot.ea_output <- function(ea, type = 'solution') {
   if (type == 'solution') {
-    nsubpop <- ga$params$nsubpop; nbands <- ga$params$nbands
-    if (ga$params$nsubpop > 1) oldpar <- par(mfrow = c(ceiling(nsubpop / 2), 2))
-    for (j in 1:nsubpop) {
-      subpop_spec <- ga$spec[, ga$labels == j, drop = FALSE]
-      matplot(ga$freq, subpop_spec,
-              type = 'l', lty = 1, lwd = 0.35, col = 'grey80',
-              ylim = c(0, max(ga$spec)),
-              xlab = 'Hz', ylab = 'Power',
-              main = ifelse(nsubpop > 1, paste0('Subpopulation ', j), 'All Replicates'))
-      abline(v = ga$endpoints[j,], lwd = 1.5, lty = 2)
-      endpoints <- c(0, ga$endpoints[j,], 0.5 * ga$params$sample_rate)
-      for (l in 1:nbands) {
-        band_freq <- ga$freq[ga$freq >= endpoints[l] & ga$freq < endpoints[l + 1]]
-        lines(band_freq, rep(ga$avg_summary[j, l], length(band_freq)),
-              col = 'red', lwd = 1.5)
-      }
-    }
-    if (ga$params$nsubpop > 1) par(oldpar)
+    `%>%` <- dplyr::`%>%`
+    nrep <- length(ea$labels)
+    nsubpop <- ea$params$nsubpop; nbands <- ea$params$nbands
+    # data frame for multitaper spectral estimates
+    mt <- cbind(as.data.frame(t(ea$spec)), ifelse(
+      rep(nsubpop > 1, nrep),
+      paste0("Subpopulation ", ea$labels),
+      rep("All Replicates", length(ea$labels))
+    ))
+    mt <- mt %>% dplyr::mutate(row_id = dplyr::row_number())
+    names(mt)[1:(ncol(mt) - 2)] <- ea$freq
+    names(mt)[ncol(mt) - 1] <- "label"
+    mt_long <- mt %>%
+      tidyr::pivot_longer(cols = -c(label, row_id),
+                          names_to = "freq", values_to = "spec") %>%
+      dplyr::mutate(freq = as.numeric(freq)) # for plotting
+
+    # data frame for internal band boundaries
+    boundaries <- data.frame(
+      label = ifelse(
+        rep(nsubpop > 1, nsubpop * (nbands - 1)),
+        paste0("Subpopulation ", rep(1:nsubpop, each = nbands - 1)),
+        rep("All Replicates", nsubpop * (nbands - 1))),
+      boundary = as.vector(t(ea$endpoints))
+    )
+
+    # data frame for collapsed measures with start and end points
+    endpoints <- matrix(min(ea$freq), nrow = nsubpop, ncol = nbands + 1)
+    endpoints[1:nsubpop, 2:nbands] <- ea$endpoints
+    endpoints[1:nsubpop, nbands + 1] <- max(ea$freq)
+    collapsed <- data.frame(
+      label = ifelse(
+        rep(nsubpop > 1, nsubpop * nbands),
+        paste0("Subpopulation ", rep(1:nsubpop, each = nbands)),
+        rep("All Replicates", nsubpop * nbands)
+      ),
+      xstart = as.vector(t(endpoints[, 1:nbands])),
+      xend = as.vector(t(endpoints[, 2:(nbands + 1)])),
+      yvalue = as.vector(t(ea$avg_summary))
+    )
+
+    p <- ggplot2::ggplot() +
+      ggplot2::geom_line(ggplot2::aes(x = freq, y = spec, group = row_id), mt_long,
+                linewidth = 0.25) +
+      ggplot2::scale_x_continuous(
+        breaks = seq(from = 0, to = 0.5 * ea$params$sample_rate, by = 0.25)
+      ) +
+      ggplot2::facet_wrap(~label) +
+      ggplot2::geom_segment(ggplot2::aes(x = xstart, xend = xend, y = yvalue, yend = yvalue),
+                   collapsed, color = 'red', size = 1) +
+      ggplot2::geom_vline(ggplot2::aes(xintercept = boundary), boundaries, linetype = 'dashed',
+                 size = 1) +
+      ggplot2::labs(x = "Hz", y = "Power") +
+      ggplot2::theme_bw()
+
+    return(p)
+
+
   } else if (type == 'history') {
-    num_gen <- length(ga$avgfit)
-    plot(1:num_gen, 1 / ga$avgfit, type = 'o', col = 'grey50',
-         ylim = c(min(c(1/ga$avgfit, 1/ga$maxfit)), max(c(1/ga$avgfit, 1/ga$maxfit))),
+    num_gen <- length(ea$avgfit)
+    plot(1:num_gen, 1 / ea$avgfit, type = 'o', col = 'grey50',
+         ylim = c(min(c(1/ea$avgfit, 1/ea$maxfit)), max(c(1/ea$avgfit, 1/ea$maxfit))),
          xlab = 'Generation Number', ylab = 'Objective Function',
          main = 'Evolution History')
-    lines(1:num_gen, 1 / ga$maxfit, type = 'o', col = 'red')
+    lines(1:num_gen, 1 / ea$maxfit, type = 'o', col = 'red')
     grid()
     legend('topright', legend = c('average objective', 'minimum objective'),
            lty = 1, col = c(1, 2))
