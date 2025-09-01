06c81857555dc6f71328d55cf2e3d571bed935e4
Connor Brubaker <141874318+brubakerconnor@users.noreply.github.com>
2025-09-01 13:48:29 -0400

    Remove nsim parameter

--- fbam_simExample.R
+++ fbam_simExample.R
@@ -4,7 +4,6 @@ source("fbam_Rfunctions.R")
 # [1] model             string    e.g., "model1", "model2a"
 # [2] nrep              integer   number of replicate time series (per subpop)
 # [3] len               integer   length of the time series epochs
-# [4] nsim              integer   number of simulation repetitions (e.g., 100)
 # [5] ncores            integer   number of cores to use for parallelization
 # [6] results           string    directory to save output (.rds file)
 # [7] run_number        integer   index in sequence of runs
@@ -12,20 +11,19 @@ source("fbam_Rfunctions.R")
 #                                 number of tapers (e.g, 0.75 = T^0.75).
 #                                 defaults to 0.5
 args <- commandArgs(trailingOnly = T)
-model <- args[1]
-nrep <- as.integer(args[2])
-len <- as.integer(args[3])
-nsim <- as.integer(args[4])
-ncores <- as.integer(args[5])
-results <- args[6]
-run_number <- args[7]
-taper_exp <- as.numeric(args[8])
+model      = args[1]
+nrep       = as.integer(args[2])
+len        = as.integer(args[3])
+ncores     = as.integer(args[4])
+results    = args[5]
+run_number = args[6]
+taper_exp  = as.numeric(args[7])
 if (is.na(taper_exp)) {
   ntapers <- floor(len ^ 0.5)
 } else {
   ntapers <- floor(len ^ taper_exp)
 }
-cat("STUDY PARAMETERS:\n", "nsim: ", nsim, "\n", "ncores: ", ncores, "\n",
+cat("STUDY PARAMETERS:\n", "ncores: ", ncores, "\n",
     "model_name: ", model, "\n", "nrep: ", nrep, "\n", "len: ", len, "\n",
     "results: ", results, "\n", "ntapers: ", ntapers, "\n")
 
